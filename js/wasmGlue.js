a93feeb9d911a6d22f4d42ca0b3a1e7ff8c1fc31
nearohen <nir.hen@gmail.com>
2024-01-11 11:23:24 +0200

    404

--- js/wasmGlue.js
+++ js/wasmGlue.js
@@ -1,4 +1,4 @@
-let improveWorker = new Worker("./improveWorker.js");
+let improveWorker = new Worker("./src/improveWorker.js");
 
 improveWorker.onmessage = function ({data :{type,args}}){
     if(type=="snapshotBuffer")
